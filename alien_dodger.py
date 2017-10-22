6612bd36d32602a81fcfcfdae1e89933e15a9719
Danial Esaid <desai001@student.ucr.edu>
2017-10-22 14:21:32 -0700

    My Second Pygame game

---
+++ alien_dodger.py
@@ -0,0 +1,94 @@
+from initialize_game import *
+
+#game variables
+number_of_enemies =                             3
+enemy_speed =                                   5
+number_of_waves =                               1
+
+#create constants for action time intervals
+new_wave_interval =                             3000
+throw_time_interval =                           650
+difficulty_time_interval =                      5000
+
+#create time variable for changing game variables
+frames_per_second_clock =                       pygame.time.Clock()         #frames per second
+hero_weapon_time =                              pygame.time.get_ticks()     #controls how often a the hero may throw a weapon
+create_enemies_time =                           pygame.time.get_ticks()     #controls frequence of enemy waves
+increase_difficulty_time =                      pygame.time.get_ticks()     #controls interval until difficulty is increased
+
+#collisions ratio callable
+collide_ratio =                                 pygame.sprite.collide_rect_ratio( 0.5 )
+
+#makes  wave of enemies
+def create_some_enemies( enemies_to_make ):
+    #creates enemy_numbers enemies
+    for alien in range( enemies_to_make ):
+        #create a new enemy at a random position
+        new_enemy_x =                           random.randint( screen_width // 2, screen_width - enemy_width )
+        new_enemy_y =                           random.randint( 0, screen_height - 60 )
+        new_enemy =                             enemy( new_enemy_x, new_enemy_y )
+        #if the new enemy is in the screen or there are 32+ enemies, add the new enemy to the enemy group
+        if  screen_rectangle.contains( new_enemy ) or enemies_to_make >= 32:
+            enemy_group.add( new_enemy )
+
+#start background music
+pygame.mixer.music.load( 'background music new.mp3' )
+pygame.mixer.music.play()
+
+#main game loop
+while True:
+
+    #for every event in the event queue
+    for event in pygame.event.get():
+        #if user exited
+        if event.type ==                        pygame.QUIT:
+            pygame.quit()
+        #moves hero according to key pressed
+        if event.type ==                        pygame.KEYDOWN: #if any key is pressed
+            if event.key == pygame.K_RIGHT:                     #if the right arrow is pressed move the hero right
+                hero.move_right()
+            elif event.key ==                   pygame.K_LEFT:  #if the left arrow is pressed move the hero left
+                hero.move_left()
+            elif event.key ==                   pygame.K_UP:    #if the up arrow is pressed move the hero up
+                hero.jump()
+            elif event.key ==                   pygame.K_DOWN:  #if the down arrow is pressed move the hero down
+                hero.fall()
+            #if the space bar is pressed and a sufficient amout of time has passed create a new hero weapon and update the time interval
+            elif event.key == pygame.K_SPACE and pygame.time.get_ticks() >= hero_weapon_time + throw_time_interval:
+                fire_sound.play()
+                hero_weapon_group.add( hero_weapon() )
+                hero_weapon_time =              pygame.time.get_ticks()
+
+    #if a sufficient amout of time has passed create a new wave of aliens and update the time interval
+    if pygame.time.get_ticks() >= create_enemies_time + new_wave_interval:
+        number_of_waves             +=          1
+        create_some_enemies( number_of_enemies )
+        create_enemies_time  =                  pygame.time.get_ticks()
+
+    #if a sufficient amount of time has passed increase the difficuly of the game
+    if  pygame.time.get_ticks() >=              increase_difficulty_time + difficulty_time_interval :
+        number_of_enemies       +=              1   #increase the number of enemies per wave by one
+        enemy_speed             +=              1   #increase the speed of the enemies by one pixel
+        increase_difficulty_time =              pygame.time.get_ticks() #update the time
+
+    #if the hero has hit any aliens fill the screen black, output the score and restart the game
+    if pygame.sprite.spritecollideany( hero, enemy_group, collided = collide_ratio ):
+        screen.blit( ending_image, top_left_of_screen )
+        ending_text =                           pygame.font.SysFont( 'Comic Sans MS' , 70 ) #next 3 lines display surface
+        ending_surface =                        ending_text.render( 'You Got To Level ' + str( number_of_waves ) + '  :D', False, white)
+        screen.blit( ending_surface, top_left_of_screen )
+        pygame.display.update()
+        time.sleep( 6 )                         #let the gamer take it in
+
+        #reset the game
+        enemy_group.empty()
+        hero_weapon_group.empty()
+        hero.rect.x =                           20
+        hero.rect.y =                           screen_height - 120
+        number_of_enemies =                     3
+        enemy_speed =                           5
+        number_of_waves =                       1
+
+    update_game( enemy_speed, number_of_waves )             #update the game
+
+    frames_per_second_clock.tick( 30 )                      #control frames per second
---
+++ initialize_game.py
@@ -0,0 +1,133 @@
+
+import pygame, random, time
+
+pygame.init()
+
+#enemy class
+class enemy( pygame.sprite.Sprite ):
+    def __init__( self, left, top ):
+        pygame.sprite.Sprite.__init__( self )
+        self.image =            enemy_image
+        self.rect =             self.image.get_rect()
+        self.rect.x =           left
+        self.rect.y =           top
+
+#Hero Class
+class hero_class( pygame.sprite.Sprite ):
+    def __init__( self, left, top ):
+        pygame.sprite.Sprite.__init__( self )
+        self.image = hero_image
+        self.rect = self.image.get_rect()
+        self.rect.x = left
+        self.rect.y = top
+    #Hero controls
+    def move_left( self ):
+        self.rect.x              -= 35
+    def move_right( self ):
+        self.rect.x              += 35
+    def jump( self ):
+        self.rect.y              -= 35
+    def fall ( self ):
+        self.rect.y              += 35
+
+#Hero weapon class
+class hero_weapon( pygame.sprite.Sprite ):
+    def __init__( self ):
+        pygame.sprite.Sprite.__init__( self )
+        self.image =            hero_weapon_image
+        self.rect =             self.image.get_rect()
+        self.rect.x =           hero.rect.x + ( hero_width // 2 )
+        self.rect.y =           hero.rect.y + ( hero_height // 2 )
+
+def update_hero_weapons():
+    #animates the hero weapons
+    for hero_throw in hero_weapon_group:
+        hero_throw.rect.x              +=  20
+    #check if any are outside the screen, if so remove it
+        if not screen_rectangle.contains( hero_throw.rect ):
+            hero_weapon_group.remove( hero_throw )
+
+def move_enemies( speed ):
+    #moves the enemies speed pixels to the right. Then check if the enemy is still in the screen, if not remove it
+    for alien in enemy_group:
+        alien.rect.x    -= speed
+    #check any enemy is outside the screen, remove it
+        if not screen_rectangle.contains( alien.rect ):
+            enemy_group.remove( alien )
+
+def correct_out_of_screen_hero():
+    #keeps hero in the screen
+    if hero.rect.right      >    screen_rectangle.right:
+        hero.rect.right     =    screen_rectangle.right
+    elif hero.rect.left     <    screen_rectangle.left:
+        hero.rect.left      =    screen_rectangle.left
+    elif hero.rect.top      <    screen_rectangle.top:
+        hero.rect.top       =    screen_rectangle.top
+    elif hero.rect.bottom   >    screen_rectangle.bottom:
+        hero.rect.bottom    =    screen_rectangle.bottom
+
+def update_time( waves ):
+    #updates time/score record
+    time_text =                  pygame.font.SysFont( 'Comic Sans MS' , 20 )
+    text_surface =               time_text.render( str( waves ), False, white)
+    screen.blit( text_surface, top_left_of_screen )
+
+#updates the game
+def update_game( speed_to_move_enemies, wave_numbers ):
+
+
+    screen.blit( background, top_left_of_screen )          #draw the background
+    enemy_group.draw( screen )                            #draw the enemies
+    update_hero_weapons()                                 #move the hero weapons
+    hero_weapon_group.draw( screen )                      #draw the hero weapons
+    move_enemies( speed_to_move_enemies )                 #update the enemies
+    pygame.sprite.groupcollide( enemy_group, hero_weapon_group, True, True )    #account for collisions between hero weapons and enemies
+    correct_out_of_screen_hero()                          #make sure the hero is in the screen
+    screen.blit( hero.image, hero.rect )                  #draw the hero
+    update_time( wave_numbers )                           #update the time tracker
+    pygame.display.update()                               #update the screen
+
+
+#colors
+white =                         ( 255, 255, 255 )
+black =                         ( 0,    0,   0  )
+
+#initialize the screen and syntactical sugar for screen
+monitor_info =                  pygame.display.Info()
+screen_width =                  monitor_info.current_w // 2
+screen_height =                 monitor_info.current_h // 2
+screen =                        pygame.display.set_mode( ( screen_width, screen_height ) )
+screen_rectangle =              screen.get_rect()
+top_left_of_screen =            ( 0, 0 )
+
+#create background and ending image
+background =                    pygame.image.load( 'background two.jpg' )
+background =                    pygame.transform.scale( background, ( screen_width, screen_height ) )
+ending_image =                  pygame.image.load( 'ending image.jpg' )
+ending_image =                  pygame.transform.scale( ending_image, ( screen_width, screen_height ) )
+
+#enemy
+enemy_width =                   screen_width // 8
+enemy_height =                  screen_height // 8
+enemy_image_original =          pygame.image.load( 'enemy.png' )
+enemy_image =                   pygame.transform.scale( enemy_image_original, ( enemy_width, enemy_height  ) )
+enemy_group =                   pygame.sprite.Group()
+
+#hero
+hero_image_original =           pygame.image.load( 'hero.png' )
+hero_width =                    screen_width // 8
+hero_height =                   screen_height // 8
+hero_image =                    pygame.transform.scale( hero_image_original, ( hero_width, hero_height ) )
+hero =                          hero_class( 20, screen_height - 120 ) #actual hero
+
+#hero weapon
+hero_weapon_image_original =    pygame.image.load( 'hero weapon.png' )
+hero_weapon_image =             pygame.transform.scale( hero_weapon_image_original, ( screen_width // 15, screen_height // 15 ) )
+hero_weapon_group =             pygame.sprite.Group()
+
+#sound effects
+fire_sound =                    pygame.mixer.Sound( 'shot.ogg' )
+fire_sound.set_volume( 1.0 )
+
+#set pygame key repeat setting
+pygame.key.set_repeat( 300, 30 )
